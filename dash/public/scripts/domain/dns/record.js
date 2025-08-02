4c1fd3d9dec2a1152ab8da5a7f92c11947973817
DrBrad <brad@bradeagle.com>
2025-08-02 14:30:55 -0600

    DropDowns

--- dash/public/scripts/domain/dns/record.js
+++ dash/public/scripts/domain/dns/record.js
@@ -2,14 +2,131 @@ const addRecordButton = document.getElementById('add-record');
 const formType = document.getElementById('form-type');
 const formTTL = document.getElementById('form-ttl');
 
+const RecordTypes = {
+    A: 'A',
+    AAAA: 'AAAA',
+    CAA: 'CAA',
+    CERT: 'CERT',
+    CNAME: 'CNAME',
+    DNSKEY: 'DNSKEY',
+    DS: 'DS',
+    HTTPS: 'HTTPS',
+    LOC: 'LOC',
+    MX: 'MX',
+    NAPTR: 'NAPTR',
+    NS: 'NS',
+    PTR: 'PTR',
+    SMIMEA: 'SMIMEA',
+    SRV: 'SRV',
+    SSHFP: 'SSHFP',
+    SVCB: 'SVCB',
+    TLSA: 'TLSA',
+    TXT: 'TXT',
+    URI: 'URI'
+};
+/*
 addRecordButton.onclick = function(e){
     console.log('CLICK');
     getPage('/fragments/add-record');
 };
+*/
 
 formType.onchange = function(e){
-    console.log(e.target.value);
+    switch(e.target.value){
+        case RecordTypes.A:
+            console.log('A');
+            break;
+
+        case RecordTypes.AAAA:
+            console.log('AAAA');
+            break;
+            
+        case RecordTypes.CAA:
+            console.log('CAA');
+            break;
+            
+        case RecordTypes.CERT:
+            console.log('CERT');
+            break;
+            
+        case RecordTypes.CNAME:
+            console.log('CNAME');
+            break;
+            
+        case RecordTypes.DNSKEY:
+            console.log('DNSKEY');
+            break;
+            
+        case RecordTypes.HTTPS:
+            console.log('HTTPS');
+            break;
+            
+        case RecordTypes.LOC:
+            console.log('LOC');
+            break;
+            
+        case RecordTypes.MX:
+            console.log('MX');
+            break;
+            
+        case RecordTypes.NAPTR:
+            console.log('NAPTR');
+            break;
+            
+        case RecordTypes.NS:
+            console.log('NS');
+            break;
+            
+        case RecordTypes.PTR:
+            console.log('PTR');
+            break;
+            
+        case RecordTypes.SMIMEA:
+            console.log('SMIMEA');
+            break;
+            
+        case RecordTypes.SRV:
+            console.log('SRV');
+            break;
+            
+        case RecordTypes.SSHFP:
+            console.log('SSHFP');
+            break;
+            
+        case RecordTypes.SVCB:
+            console.log('SVCB');
+            break;
+            
+        case RecordTypes.TLSA:
+            console.log('TLSA');
+            break;
+            
+        case RecordTypes.TXT:
+            console.log('TXT');
+            break;
+            
+        case RecordTypes.URI:
+            console.log('URI');
+            break;
+    }
 };
 
+function createARecord(){
+    const address = createTextField({
+        label: 'IPv4 Address',
+        name: 'address'
+    });
+
+
+}
+
+function createAAAARecord(){
+    const address = createTextField({
+        label: 'IPv6 Address',
+        name: 'address'
+    });
+
+    
+}
 
 
--- dash/public/scripts/form.js
+++ dash/public/scripts/form.js
@@ -1,26 +1,47 @@
 (function(){
+    const textFields = document.querySelectorAll('text-field');
+    for(let textField of textFields){
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.name = textField.getAttribute('name');
+        textField.appendChild(input);
+    }
+
     const selects = document.querySelectorAll('fl-select');
 
-    for(let s of selects){
-        s.setAttribute('tabindex', '0');
-        const firstOption = s.querySelector('fl-option');
+    for(let select of selects){
+        select.setAttribute('tabindex', '0');
+        const firstOption = select.querySelector('fl-option');
         const input = document.createElement('input');
         input.type = 'hidden';
-        input.name = s.getAttribute('name', s.name);
+        input.name = select.getAttribute('name');
         input.value = firstOption.getAttribute('value');
-        s.parentElement.insertBefore(input, s);
+        select.parentElement.insertBefore(input, select);
 
-        s.setAttribute('label', firstOption.textContent);
-        s.onclick = function(e){
+        select.setAttribute('label', firstOption.textContent);
+        select.onclick = function(e){
             if(e.target.tagName == 'FL-OPTION'){
                 input.value = e.target.getAttribute('value');
-                s.value = e.target.getAttribute('value');
-                s.setAttribute('label', e.target.textContent);
-                s.dispatchEvent(new Event('change'));
-                s.blur();
+                select.value = e.target.getAttribute('value');
+                select.setAttribute('label', e.target.textContent);
+                select.dispatchEvent(new Event('change'));
+                select.blur();
                 return;
             }
-            s.focus();
+            select.focus();
         };
     }
 })();
+
+function createTextField(data){
+    const textField = document.createElement('text-field');
+    textField.setAttribute('label', data.label);
+    textField.setAttribute('name', data.name);
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.name = data.name;
+    textField.appendChild(input);
+
+    return textField;
+}
